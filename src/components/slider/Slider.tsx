317af0a5dbf24b0550770f3f6f643cca0ca8b421
krishna pradhan <kpaccess@gmail.com>
2024-08-29 15:55:44 -0400

    added address and images in single page

---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,7 @@
+import "./slider.scss";
+
+const Slider = ({ images }: { images: string[] }) => {
+  console.log(images);
+  return <div>Slider</div>;
+};
+export default Slider;
--- src/routes/singlePage/singlePage.tsx
+++ src/routes/singlePage/singlePage.tsx
@@ -1,4 +1,36 @@
+import Slider from "../../components/slider/Slider";
+import { singlePostData, userData } from "../../lib/dummydata";
+import "./singlePage.scss";
+
 const SinglePage = () => {
-  return <div className="single-page">SinglePage</div>;
+  return (
+    <div className="single-page">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="pin" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">$ {singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="user data" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">{singlePostData.description}</div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
 };
 export default SinglePage;
