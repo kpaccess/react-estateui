a59a8f0432cbe537a0feaca598262b8d29412bc8
krishna pradhan <kpaccess@gmail.com>
2024-08-29 17:48:49 -0400

    add images for slider

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,7 +1,34 @@
 import "./slider.scss";
 
-const Slider = ({ images }: { images: string[] }) => {
-  console.log(images);
-  return <div>Slider</div>;
+type ImageProps = {
+  images: string[];
+};
+
+const Slider = ({ images }: ImageProps) => {
+  const firstImage: string = images[0];
+  return (
+    <div className="slider">
+      <div className="full-slider">
+        <div className="arrow">
+          <img src="./arrow.png" alt="" />
+        </div>
+        <div className="image-container">
+          <img src={firstImage} alt="" />
+        </div>
+        <div className="arrow">
+          <img src="./arrow.png" className="right" alt="" />
+        </div>
+        <div className="close">X</div>
+      </div>
+      <div className="big-image">
+        <img src={firstImage} alt="" />
+      </div>
+      <div className="small-images">
+        {images.slice(1).map((image, index) => (
+          <img key={index} src={image} alt="" />
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Slider;
